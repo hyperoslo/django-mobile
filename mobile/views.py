8415444152187662ca8d1fcc6215e29e3684406e
Espen Høgbakk <espen@hogbakk.no>
2011-09-13 17:45:29 +0200

    Add csrf_exempt to receive sms view

--- mobile/views.py
+++ mobile/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from django_api_utilities.decorators import methods_supported, authentication_required
 
@@ -7,6 +8,7 @@ from settings import GATE_USERNAME, GATE_PASSWORD
 
 @methods_supported(['POST'])
 #@authentication_required(GATE_USERNAME, GATE_PASSWORD)
+@csrf_exempt
 def receive_sms(request):
     backend.SMS.receive(request.raw_post_data)
     
