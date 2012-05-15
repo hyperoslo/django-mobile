91dd428ef8700ba1a61457987d3da78c18d336f7
Tim Kurvers <tim@moonsphere.net>
2012-05-15 13:31:39 +0200

    Removed dependency on django_api_utilities; POST-method is now ensured

--- mobile/views.py
+++ mobile/views.py
@@ -1,20 +1,21 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from django_api_utilities.decorators import methods_supported, authentication_required
-
 from backends import backend
 from settings import GATE_USERNAME, GATE_PASSWORD
 
-@methods_supported(['POST'])
-#@authentication_required(GATE_USERNAME, GATE_PASSWORD)
 @csrf_exempt
 def receive_sms(request):
+    if request.method != 'POST':
+        return HttpResponse(status=405)
+    
     backend.SMS.receive(request.raw_post_data)
-    
     return HttpResponse(status=200)
-    
-def receive_mms(request):
-    backend.MMS.receive(request.raw_post_data)
 
+@csrf_exempt
+def receive_mms(request):
+    if request.method != 'POST':
+        return HttpResponse(status=405)
+    
+    backend.MMS.receive(request.raw_post_data)
     return HttpResponse(status=200)
